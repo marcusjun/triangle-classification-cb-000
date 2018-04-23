104f72401e3dc6089941068b55550b9e23ea87a2
Marcus Jun <mjun@calstate.edu>
2018-04-23 21:12:21 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,8 +9,10 @@ class Triangle
   end
 
   def kind
-
+    
     #if (side1<=0 || side2<=0 || side3<=0 || (side1+side2<=side3) || (side2+side3<=side1) || (side3+side1<=side2))
+
+    #instead of using a long conditional statement, using methods instead
     if less_than_or_equal_to_zero || side_sum
       raise TriangleError
     elsif (side1==side2 && side2==side3)
