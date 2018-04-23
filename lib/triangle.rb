e4a56ea7cb70f0461031c5be4abd4a7d695a4d2b
Marcus Jun <mjun@calstate.edu>
2018-04-23 22:09:35 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,6 @@
 class Triangle
   # write code here
-  attr_accessor :side1, :side2, :side3
+  attr_reader :side1, :side2, :side3
 
   def initialize (side1, side2, side3)
     @side1=side1
@@ -10,7 +10,10 @@ class Triangle
 
   def kind
 
-    if (side1<=0 || side2<=0 || side3<=0 || (side1+side2<=side3) || (side2+side3<=side1) || (side3+side1<=side2))
+    #if (side1<=0 || side2<=0 || side3<=0 || (side1+side2<=side3) || (side2+side3<=side1) || (side3+side1<=side2))
+
+    #instead of using a long conditional statement, using methods instead
+    if less_than_or_equal_to_zero || side_sum
       raise TriangleError
     elsif (side1==side2 && side2==side3)
       :equilateral
@@ -21,9 +24,13 @@ class Triangle
     end
   end
 
-  #def less_than_or_equal_to_zero
-    #side<=0
-  #end
+  def less_than_or_equal_to_zero
+    side1 <=0 || side2 <=0 || side3<=0
+  end
+
+  def side_sum
+    (side1+side2<=side3) || (side2+side3<=side1) || (side3+side1<=side2)
+  end
 
   class TriangleError < StandardError
     def message
